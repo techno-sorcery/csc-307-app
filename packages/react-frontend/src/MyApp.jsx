de49e9a9ea42e5428a253096b78982e55273bbd7
techno-sorcery <inquisitivecalculator@gmail.com>
2025-01-14 18:07:35 -0800

    Updated stuff

---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,44 @@
+// src/MyApp.jsx
+import React, { useState } from "react"
+import Table from "./Table"
+
+
+function MyApp() {
+    const [characters, setCharacters] = useState([
+        {
+            name: "Charlie",
+            job: "Janitor"
+        },
+        {
+            name: "Mac",
+            job: "Bouncer"
+        },
+        {
+            name: "Dee",
+            job: "Aspring actress"
+        },
+        {
+            name: "Dennis",
+            job: "Bartender"
+        }
+    ]);
+
+    return (
+        <div className="container">
+            <Table 
+                characterData={characters}
+                removeCharacter={removeOneCharacter}
+            />
+        </div>
+    );
+
+    function removeOneCharacter(index) {
+        const updated = characters.filter((character, i) => {
+            return i !== index;
+        });
+        
+        setCharacters(updated);
+    }
+}
+
+export default MyApp;
