b6d76a285431479098ab98966beefffe47c69e03
techno-sorcery <inquisitivecalculator@gmail.com>
2025-01-20 15:08:31 -0800

    Created backend

---
+++ packages/express-backend/backend.js
@@ -0,0 +1,51 @@
+// backend.js
+
+import express from "express";
+
+const app = express();
+const port = 8000;
+
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor"
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]
+};
+
+
+app.get("/users", (req, res) => {
+    res.send(users);
+});
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
