5fc94c47447db1cee872e9a3c1cc58922ced5e77
techno-sorcery <inquisitivecalculator@gmail.com>
2025-01-22 16:52:49 -0800

    Added job search functionality

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -38,9 +38,16 @@ const users = {
 };
 
 const findUserByName = (name) => {
-    return users["users_list"].filter(
-        (user) => user["name"] === name
-    );
+    return users["users_list"].filter((user) => user["name"] === name);
+};
+
+const findUserByJob = (job) => {
+    return users["users_list"].filter((user) => user["job"] === job);
+};
+
+const findUserByNameAndJob = (name, job) => {
+    return users["users_list"].filter((user) => user["name"] === name)
+    .filter((user) => user["job"] === job);
 };
 
 const findUserById = (id) => {
@@ -77,9 +84,23 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
     const name = req.query.name;
+    const job = req.query.job;
 
-    if (name != undefined) {
+    // Name and job
+    if (name != undefined && job != undefined) {
+        let result = findUserByNameAndJob(name, job);
+        result = { users_list: result };
+        res.send(result);
+
+    // Name
+    } else if (name != undefined) {
         let result = findUserByName(name);
+        result = { users_list: result };
+        res.send(result);
+
+    // Job
+    } else if (job != undefined) {
+        let result = findUserByJob(job);
         result = { users_list: result };
         res.send(result);
 
